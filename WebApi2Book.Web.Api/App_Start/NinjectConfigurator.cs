50e974dbcad38f39d50b4fa83a5a1fd9712a53ee
Ladi <ladipoilori@yahoo.com>
2016-02-14 19:59:55 +0000

    Implemented configuration for NHibernate

--- WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
+++ WebApi2Book.Web.Api/App_Start/NinjectConfigurator.cs
@@ -2,6 +2,13 @@
 using Ninject;
 using WebApi2Book.Common;
 using WebApi2Book.Common.Logging;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Context;
+using Ninject.Activation;
+using Ninject.Web.Common;
+using WebApiBook.Data.SqlServer.Mapping;
 
 namespace WebApi2Book.Web.Api
 {
@@ -15,6 +22,7 @@ namespace WebApi2Book.Web.Api
         private static void AddBinding(IKernel container)
         {
             ConfigureLog4Net(container);
+            ConfigureNHibernate(container);
             container.Bind<IDateTime>().To<DateTImeAdapter>().InSingletonScope();
         }
 
@@ -24,6 +32,31 @@ namespace WebApi2Book.Web.Api
 
             var logManager = new LogManagerAdapter();
             container.Bind<ILogManager>().ToConstant(logManager);
+        }
+
+        private static void ConfigureNHibernate(IKernel container)
+        {
+            var sessionFactory = Fluently.Configure()
+                .Database(
+                    MsSqlConfiguration.MsSql2008.ConnectionString(c => c.FromConnectionStringWithKey("WebApi2BookDb")))
+                .CurrentSessionContext("web")
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<TaskMap>())
+                .BuildSessionFactory();
+
+            container.Bind<ISessionFactory>().ToConstant(sessionFactory);
+            container.Bind<ISession>().ToMethod(CreateSession).InRequestScope();
+        }
+
+        private static ISession CreateSession(IContext context)
+        {
+            var sessionFactory = context.Kernel.Get<ISessionFactory>();
+            if (!CurrentSessionContext.HasBind(sessionFactory))
+            {
+                var session = sessionFactory.OpenSession();
+                CurrentSessionContext.Bind(session);
+            }
+
+            return sessionFactory.GetCurrentSession();
         }
     }
 }
